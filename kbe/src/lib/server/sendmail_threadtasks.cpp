76975ca4f1d0d3e9a67fd21be7dc580661eb6ead
yqs <yiquanshui@sina.com>
2024-05-21 16:08:46 +0800

    commnet out jwsmtp usage

--- kbe/src/lib/server/sendmail_threadtasks.cpp
+++ kbe/src/lib/server/sendmail_threadtasks.cpp
@@ -1,6 +1,6 @@
 // Copyright 2008-2018 Yolo Technologies, Inc. All Rights Reserved. https://www.comblockengine.com
 
-#include "jwsmtp.h"
+//#include "jwsmtp.h"
 #include "sendmail_threadtasks.h"
 #include "server/serverconfig.h"
 #include "common/deadline.h"
@@ -10,31 +10,31 @@ namespace KBEngine{
 //-------------------------------------------------------------------------------------
 bool SendEMailTask::process()
 {
-	jwsmtp::mailer m(emailaddr_.c_str(), g_kbeSrvConfig.emailServerInfo_.username.c_str(), subject(),
-		g_kbeSrvConfig.emailAtivationInfo_.subject.c_str(), g_kbeSrvConfig.emailServerInfo_.smtp_server.c_str(),
-		g_kbeSrvConfig.emailServerInfo_.smtp_port, false);
-
-	m.authtype(g_kbeSrvConfig.emailServerInfo_.smtp_auth == 1 ? jwsmtp::mailer::LOGIN : jwsmtp::mailer::PLAIN);  
-
-	std::string mailmessage = message();
-
-	KBEngine::strutil::kbe_replace(mailmessage, "${backlink}", fmt::format("http://{}:{}/{}_{}", 
-		cbaddr_,
-		cbport_,
-		getopkey(),
-		code_));
-
-	KBEngine::strutil::kbe_replace(mailmessage, "${username}", emailaddr_);
-	KBEngine::strutil::kbe_replace(mailmessage, "${code}", code_);
-	mailmessage = KBEngine::strutil::kbe_trim(mailmessage);
-
-	m.setmessageHTML(mailmessage);
-
-	m.username(g_kbeSrvConfig.emailServerInfo_.username.c_str());
-	m.password(g_kbeSrvConfig.emailServerInfo_.password.c_str());
-	m.send(); // send the mail
-
-	INFO_MSG(fmt::format("SendEMailTask::process: sendmail[{}]: {}\n", getopkey(), m.response()));
+//	jwsmtp::mailer m(emailaddr_.c_str(), g_kbeSrvConfig.emailServerInfo_.username.c_str(), subject(),
+//		g_kbeSrvConfig.emailAtivationInfo_.subject.c_str(), g_kbeSrvConfig.emailServerInfo_.smtp_server.c_str(),
+//		g_kbeSrvConfig.emailServerInfo_.smtp_port, false);
+//
+//	m.authtype(g_kbeSrvConfig.emailServerInfo_.smtp_auth == 1 ? jwsmtp::mailer::LOGIN : jwsmtp::mailer::PLAIN);
+//
+//	std::string mailmessage = message();
+//
+//	KBEngine::strutil::kbe_replace(mailmessage, "${backlink}", fmt::format("http://{}:{}/{}_{}",
+//		cbaddr_,
+//		cbport_,
+//		getopkey(),
+//		code_));
+//
+//	KBEngine::strutil::kbe_replace(mailmessage, "${username}", emailaddr_);
+//	KBEngine::strutil::kbe_replace(mailmessage, "${code}", code_);
+//	mailmessage = KBEngine::strutil::kbe_trim(mailmessage);
+//
+//	m.setmessageHTML(mailmessage);
+//
+//	m.username(g_kbeSrvConfig.emailServerInfo_.username.c_str());
+//	m.password(g_kbeSrvConfig.emailServerInfo_.password.c_str());
+//	m.send(); // send the mail
+//
+//	INFO_MSG(fmt::format("SendEMailTask::process: sendmail[{}]: {}\n", getopkey(), m.response()));
 	return false;
 }
 
